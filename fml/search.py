97042b7c61c97c12183f81dd395e1f912a3a9ab5
Nicolas Hug <contact@nicolas-hug.com>
2019-02-08 12:11:20 -0500

    bunch minor improvements:
    - use super() in BaseSuccessiveHalving
    - allow random_state for subsampling data
    - more efficient subsampling

--- fml/search.py
+++ fml/search.py
@@ -10,7 +10,7 @@ from sklearn.model_selection._search import _fit_and_score
 from sklearn.model_selection import ParameterGrid, ParameterSampler
 from sklearn.utils.validation import indexable
 from sklearn.metrics.scorer import _check_multimetric_scoring
-from sklearn.utils import shuffle
+from sklearn.utils import shuffle, check_random_state
 from sklearn.model_selection._split import check_cv
 from sklearn.model_selection._validation import _aggregate_score_dicts
 from sklearn.base import is_classifier
@@ -26,27 +26,30 @@ class BaseSuccessiveHalving(BaseSearchCV):
     Almost optimal exploration in multi-armed bandits, ICML 13
     Zohar Karnin, Tomer Koren, Oren Somekh
     """
-    def __init__(self, estimator, scoring=None,
-                 n_jobs=None, refit=True, verbose=0, cv=None,
-                 pre_dispatch='2*n_jobs'):
+    def __init__(self, estimator, scoring,
+                 n_jobs=None, refit=True, cv=None, verbose=0,
+                 pre_dispatch='2*n_jobs', random_state=None,
+                 error_score=np.nan, return_train_score=True):
+        super().__init__(estimator, scoring=scoring,
+                 n_jobs=n_jobs, refit=refit, cv=cv, verbose=verbose,
+                 pre_dispatch=pre_dispatch, error_score=error_score,
+                 return_train_score=return_train_score)
 
-        self.estimator = estimator
-        self.scoring = scoring  # FIXME: support this?
         if scoring is not None:
             raise NotImplementedError
-        self.n_jobs = n_jobs
-        self.refit = refit  # FIXME
         if refit is not True:
-            raise NotImplementedError
-        self.verbose = verbose
-        self.cv = cv
-        self.pre_dispatch = pre_dispatch
+            raise NotImplemented
+
+        self.random_state = random_state
+
 
     def fit(self, X, y, groups=None, **fit_params):
 
         X, y, groups = indexable(X, y, groups)
         cv = check_cv(self.cv, y, classifier=is_classifier(self.estimator))
         n_splits = cv.get_n_splits(X, y, groups)
+
+        rng = check_random_state(self.random_state)
 
         base_estimator = clone(self.estimator)
         parallel = Parallel(n_jobs=self.n_jobs, verbose=self.verbose,
@@ -56,11 +59,11 @@ class BaseSuccessiveHalving(BaseSearchCV):
             self.estimator, scoring=self.scoring)
         fit_and_score_kwargs = dict(scorer=scorers,
                                     fit_params=fit_params,
-                                    return_train_score=False,
+                                    return_train_score=self.return_train_score,
                                     return_n_test_samples=True,
                                     return_times=True,
                                     return_parameters=False,
-                                    error_score=np.nan,
+                                    error_score=self.error_score,
                                     verbose=self.verbose)
 
         candidate_params = list(self._generate_candidate_params())
@@ -68,11 +71,13 @@ class BaseSuccessiveHalving(BaseSearchCV):
         n_samples_total = X.shape[0]
 
         for _ in range(n_iterations):
+            # randomly sample training samples
             n_candidates = len(candidate_params)
-            n_samples = floor(n_samples_total / (n_candidates * n_iterations))
-            # FIXME: use shuffle split instead?
-            shuffle(X, y)
-            X_iter, y_iter = X[:n_samples], y[:n_samples]
+            n_samples_iter = floor(n_samples_total /
+                                   (n_candidates * n_iterations))
+            indices = rng.choice(n_samples_total, n_samples_iter,
+                                 replace=False)
+            X_iter, y_iter = X[indices], y[indices]
 
             out = parallel(delayed(_fit_and_score)(clone(base_estimator),
                                                    X, y,
@@ -83,7 +88,8 @@ class BaseSuccessiveHalving(BaseSearchCV):
                            in product(candidate_params,
                                       cv.split(X_iter, y_iter)))
 
-            test_score_dicts = [res[0] for res in out]
+            test_score_index = self.return_train_score  # 1 or 0
+            test_score_dicts = [res[test_score_index] for res in out]
             test_scores = _aggregate_score_dicts(test_score_dicts)['score']
             test_scores = test_scores.reshape(n_candidates, n_splits)
             avg_test_scores = test_scores.mean(axis=1)
@@ -110,10 +116,13 @@ class GridSuccessiveHalving(BaseSuccessiveHalving):
 
     def __init__(self, estimator, param_grid, scoring=None,
                  n_jobs=None, refit=True, verbose=0, cv=None,
-                 pre_dispatch='2*n_jobs'):
+                 pre_dispatch='2*n_jobs', random_state=None,
+                 error_score=np.nan, return_train_score=True):
         super().__init__(estimator, scoring=scoring,
                          n_jobs=n_jobs, refit=refit, verbose=verbose, cv=cv,
-                         pre_dispatch=pre_dispatch)
+                         pre_dispatch=pre_dispatch,
+                         random_state=random_state, error_score=error_score,
+                         return_train_score=return_train_score)
         self.param_grid = param_grid
         _check_param_grid(self.param_grid)
 
@@ -125,13 +134,14 @@ class RandomSuccessiveHalving(BaseSuccessiveHalving):
 
     def __init__(self, estimator, param_distributions, n_iter=10, scoring=None,
                  n_jobs=None, refit=True, verbose=0, cv=None,
-                 pre_dispatch='2*n_jobs', random_state=None):
+                 pre_dispatch='2*n_jobs', random_state=None,
+                 error_score=np.nan, return_train_score=True):
         super().__init__(estimator, scoring=scoring,
                          n_jobs=n_jobs, refit=refit, verbose=verbose, cv=cv,
-                         pre_dispatch=pre_dispatch)
+                         random_state=random_state, error_score=error_score,
+                         return_train_score=return_train_score)
         self.param_distributions = param_distributions
         self.n_iter = n_iter
-        self.random_state = random_state
 
     def _generate_candidate_params(self):
         return ParameterSampler(self.param_distributions, self.n_iter,
--- fml/tests/test_search.py
+++ fml/tests/test_search.py
@@ -11,11 +11,11 @@ base_estimator = SVC(gamma='scale')
 
 
 @pytest.mark.parametrize('sh', (
-    GridSuccessiveHalving(base_estimator, parameters, cv=5),
-    RandomSuccessiveHalving(base_estimator, parameters, cv=5, n_iter=4,
-                            random_state=0)
+    GridSuccessiveHalving(base_estimator, parameters),
+    RandomSuccessiveHalving(base_estimator, parameters, n_iter=4)
 ))
 def test_basic(sh):
     X, y = make_classification(n_samples=1000, random_state=0)
+    sh.set_params(random_state=0, cv=5)
     sh.fit(X, y)
     assert sh.score(X, y) > .98

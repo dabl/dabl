facb7dda1d2238d25a9310481d37fa53316b27b6
Andreas Mueller <andreas.mueller@columbia.edu>
2019-03-20 10:33:07 -0400

    polish continuous regression plots

--- fml/plotting.py
+++ fml/plotting.py
@@ -14,6 +14,12 @@ def plot_categorical_unsupervised(X):
     pass
 
 
+def _shortname(some_string):
+    if len(some_string) > 20:
+        return some_string[:17] + "..."
+    else:
+        return some_string
+
 def plot_unsupervised(X, verbose=10):
     types = detect_types_dataframe(X)
     # if any dirty floats, tell user to clean them first
@@ -22,14 +28,15 @@ def plot_unsupervised(X, verbose=10):
     plot_categorical_unsupervised(X.loc[:, types.categorical])
 
 
-def plot_regression_continuous(X, target):
+def plot_regression_continuous(X, target_col):
     if X.shape[1] > 20:
+        print("Showing only top 10 of {} continuous features".format(X.shape[1]))
         # too many features, show just top 10
         show_top = 10
     else:
         show_top = X.shape[1]
-    features = X.drop(target, axis=1)
-    target = X[target]
+    features = X.drop(target_col, axis=1)
+    target = X[target_col]
     # HACK we should drop them per column before feeding them into f_regression
     # FIXME
     features_imp = SimpleImputer().fit_transform(features)
@@ -37,15 +44,25 @@ def plot_regression_continuous(X, target):
     top_k = np.argsort(f)[-show_top:][::-1]
     # we could do better lol
     n_cols = 5
-    fig, axes = plt.subplots(int(np.ceil(show_top / n_cols)), n_cols, figsize=(20, 5))
-    for col, ax in zip(top_k, axes.ravel()):
+    n_rows = int(np.ceil(show_top / n_cols))
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=(20, 3 * n_rows),
+                             constrained_layout=True)
+    plt.suptitle("Continuous Target Plots")
+    for i, (col, ax) in enumerate(zip(top_k, axes.ravel())):
+        if i % n_cols == 0:
+            ax.set_ylabel(target_col)
         ax.plot(features.iloc[:, col], target, 'o', alpha=.6)
-        ax.set_title("{}, {:.2f}".format(_shortname(features.columns[col]), f[col]))
+        ax.set_xlabel(_shortname(features.columns[col]))
+        ax.set_title("F={:.2E}".format(f[col]))
 
+    for j in range(i + 1, n_rows * n_cols):
+        # turn off axis if we didn't fill last row
+        axes.ravel()[j].set_axis_off()
 
 
 def plot_regression_categorical(X, target):
     pass
+
 
 def plot_supervised(X, target, verbose=10):
     types = detect_types_dataframe(X)

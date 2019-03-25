31a92be13b83639f3af70599a2cf5839a32ade79
Nicolas Hug <contact@nicolas-hug.com>
2019-03-25 19:16:45 -0400

    lots of changes... still very WIP

--- fml/search.py
+++ fml/search.py
@@ -1,11 +1,14 @@
-from math import ceil, floor, log2
+from math import ceil, floor, log
 from abc import abstractmethod
 
 import numpy as np
 from sklearn.model_selection._search import BaseSearchCV
 from sklearn.model_selection._search import _check_param_grid
 from sklearn.model_selection import ParameterGrid, ParameterSampler
+from sklearn.model_selection import train_test_split
 from sklearn.utils import check_random_state
+from sklearn.base import is_classifier
+from sklearn.model_selection._split import check_cv
 
 
 __all__ = ['GridSuccessiveHalving', 'RandomSuccessiveHalving']
@@ -30,10 +33,13 @@ class BaseSuccessiveHalving(BaseSearchCV):
     Almost optimal exploration in multi-armed bandits, ICML 13
     Zohar Karnin, Tomer Koren, Oren Somekh
     """
-    def __init__(self, estimator, scoring,
+    def __init__(self, estimator, scoring=None,
                  n_jobs=None, refit=True, cv=None, verbose=0,
                  pre_dispatch='2*n_jobs', random_state=None,
-                 error_score=np.nan, return_train_score=True):
+                 error_score=np.nan, return_train_score=True,
+                 max_budget='auto', budget_on='n_samples', ratio=3,
+                 r_min='auto', aggressive_elimination=False,
+                 exhaust_budget=False):
 
         refit = _refit_callable if refit else False
         super().__init__(estimator, scoring=scoring,
@@ -43,43 +49,141 @@ class BaseSuccessiveHalving(BaseSearchCV):
                          return_train_score=return_train_score, iid=False)
 
         self.random_state = random_state
+        self.max_budget = max_budget
+        self.budget_on = budget_on
+        self.ratio = ratio
+        self.r_min = r_min
+        self.aggressive_elimination = aggressive_elimination
+        self.exhaust_budget = exhaust_budget
+
+    def _check_input_parameters(self, X, y, n_candidates):
+
+        if self.budget_on != 'n_samples':
+            raise ValueError('budget_on must be n_samples for now')
+
+        self.r_min_ = self.r_min
+        if self.r_min_ == 'auto':
+            if self.budget_on == 'n_samples':
+                cv = check_cv(self.cv, y,
+                              classifier=is_classifier(self.estimator))
+                n_splits = cv.get_n_splits(X, y)  # FIXME: should pass group :/
+
+                magic_factor = 2
+                self.r_min_ = n_splits * magic_factor
+                if is_classifier(self.estimator):
+                    n_classes = np.unique(y).shape[0]
+                    self.r_min_ *= n_classes
+            else:
+                self.r_min_ = 1
+
+        self.max_budget_ = self.max_budget
+        if self.max_budget_ == 'auto':
+            self.max_budget_ = X.shape[0]  # only budget_on=n_samples allowed
+
+        # We want resources to be in units of r_min_
+        self.max_budget_ = self.max_budget_ // self.r_min_
 
     def _run_search(self, evaluate_candidates, X, y):
-        rng = check_random_state(self.random_state)
 
         candidate_params = list(self._generate_candidate_params())
-        n_iterations = int(ceil(log2(len(candidate_params))))
-        n_samples_total = X.shape[0]
+        n_candidates = len(candidate_params)
+
+        self._check_input_parameters(
+            X=X,
+            y=y,
+            n_candidates=n_candidates
+        )
+        rng = check_random_state(self.random_state)
+
+        # n_iterations_required is the number of iterations needed so that the
+        # last iterations evaluates less than `ratio` candidates.
+        n_required_iterations = floor(log(n_candidates, self.ratio)) + 1
+
+        actual_max_resources = self.max_budget_ * self.r_min_
+        last_iter = n_required_iterations - 1
+        r_last_iter = self.ratio**last_iter * self.r_min_
+        if self.exhaust_budget:
+            # To exhaust we want to start with the biggest
+            # r_min possible so that the last iteration uses as much resources
+            # as possible
+            self.r_min_ = actual_max_resources // self.ratio**last_iter
+
+
+        # n_possible iterations is the number of iterations that we can
+        # actually do starting from r_min and without exceeding the budget.
+        n_possible_iterations = floor(log(self.max_budget_, self.ratio)) + 1
+
+        print(f'n_required_iterations: {n_required_iterations}')
+        print(f'n_possible_iterations: {n_possible_iterations}')
+        print(f'r_min: {self.r_min_}')
+        print(f'max_budget_: {self.max_budget_}')
+        print(f'aggressive_elimination: {self.aggressive_elimination}')
+        print(f'ratio: {self.ratio}')
+
+        self._r_i_list = []
+
+
+        if self.aggressive_elimination:
+            n_iterations = n_required_iterations
+        else:
+            n_iterations = min(n_possible_iterations, n_required_iterations)
 
         for iter_i in range(n_iterations):
-            # randomly sample training samples
+
+            power = iter_i  # default
+            if self.aggressive_elimination:
+                power = max(0, iter_i - n_required_iterations + n_possible_iterations)
+
+            r_i = int(self.ratio**power * self.r_min_)
+            r_i = min(r_i, self.max_budget_ * self.r_min_)
+            self._r_i_list.append(r_i)
+
             n_candidates = len(candidate_params)
-            n_samples_iter = floor(n_samples_total /
-                                   (n_candidates * n_iterations))
-            indices = rng.choice(n_samples_total, n_samples_iter,
-                                 replace=False)
-            X_iter, y_iter = X[indices], y[indices]
+            print('-' * 10)
+            print(f'iter_i: {iter_i}')
+            print(f'n_candidates: {n_candidates}')
+            print(f'r_i: {r_i}')
+            print(f'r_i (in r_min units): {r_i // self.r_min_}')
+
+            if self.budget_on == 'n_samples':
+                # ideally use train_test_split, but doesn't work lololollol
+                # stratify = y if is_classifier(self.estimator) else None
+                # X_iter, _, y_iter, _ = train_test_split(
+                #     X, y, stratify=stratify, train_size=r_i, test_size=None,
+                #     random_state=rng)
+                indexes = rng.choice(r_i, X.shape[0])
+                X_iter, y_iter = X[indexes], y[indexes]
+            else:
+                raise ValueError("I TOLD YOU NOT TO")
 
             more_results= {'iter': [iter_i] * n_candidates,
-                           'n_samples': [n_samples_iter] * n_candidates}
-            out = evaluate_candidates(candidate_params, X_iter, y_iter,
-                                      more_results=more_results)
+                           'r_i': [r_i] * n_candidates}
+            results = evaluate_candidates(candidate_params, X_iter, y_iter,
+                                          more_results=more_results)
 
-            # Select the best half of the candidates for the next iteration
-            # We need to filter out candidates from the previous iterations
-            n_candidates_to_keep = ceil(n_candidates / 2)
-            best_candidates_indices = np.argsort(out['mean_test_score'])[::-1]
-            best_candidates_indices = [i for i in best_candidates_indices
-                                       if out['iter'][i] == iter_i]
-            best_candidates_indices = \
-                best_candidates_indices[:n_candidates_to_keep]
-            candidate_params = [out['params'][i]
-                                for i in best_candidates_indices]
+            n_candidates_to_keep = ceil(n_candidates/ self.ratio)
+            candidate_params = self._keep_k_best(results,
+                                                 n_candidates_to_keep,
+                                                 iter_i)
 
-        assert len(candidate_params) == n_candidates_to_keep == 1
+        self.n_required_iterations_ = n_required_iterations
+        self.n_possible_iterations_ = n_possible_iterations
+        self.n_iterations_ = n_iterations
+
+
+    def _keep_k_best(self, results, k, iter_i):
+        # Select the best candidates of a given iteration
+        # We need to filter out candidates from the previous iterations
+        # when sorting
+
+        best_candidates_indices = np.argsort(results['mean_test_score'])[::-1]
+        best_candidates_indices = [idx for idx in best_candidates_indices
+                                   if results['iter'][idx] == iter_i]
+        best_candidates_indices = best_candidates_indices[:k]
+        return [results['params'][idx] for idx in best_candidates_indices]
 
     @abstractmethod
-    def _generate_candidate_params():
+    def _generate_candidate_params(self):
         pass
 
 
@@ -88,12 +192,19 @@ class GridSuccessiveHalving(BaseSuccessiveHalving):
     def __init__(self, estimator, param_grid, scoring=None,
                  n_jobs=None, refit=True, verbose=0, cv=None,
                  pre_dispatch='2*n_jobs', random_state=None,
-                 error_score=np.nan, return_train_score=True):
+                 error_score=np.nan, return_train_score=True,
+                 max_budget='auto', budget_on='n_samples', ratio=3,
+                 r_min='auto', aggressive_elimination=False,
+                 exhaust_budget=False):
         super().__init__(estimator, scoring=scoring,
                          n_jobs=n_jobs, verbose=verbose, cv=cv,
                          pre_dispatch=pre_dispatch,
                          random_state=random_state, error_score=error_score,
-                         return_train_score=return_train_score)
+                         return_train_score=return_train_score,
+                         max_budget=max_budget, budget_on=budget_on,
+                         ratio=ratio, r_min=r_min,
+                         aggressive_elimination=aggressive_elimination,
+                         exhaust_budget=exhaust_budget)
         self.param_grid = param_grid
         _check_param_grid(self.param_grid)
 
@@ -106,11 +217,18 @@ class RandomSuccessiveHalving(BaseSuccessiveHalving):
     def __init__(self, estimator, param_distributions, n_iter=10, scoring=None,
                  n_jobs=None, refit=True, verbose=0, cv=None,
                  pre_dispatch='2*n_jobs', random_state=None,
-                 error_score=np.nan, return_train_score=True):
+                 error_score=np.nan, return_train_score=True,
+                 max_budget='auto', budget_on='n_samples', ratio=3,
+                 r_min='auto', aggressive_elimination=False,
+                 exhaust_budget=False):
         super().__init__(estimator, scoring=scoring,
                          n_jobs=n_jobs, verbose=verbose, cv=cv,
                          random_state=random_state, error_score=error_score,
-                         return_train_score=return_train_score)
+                         return_train_score=return_train_score,
+                         max_budget=max_budget, budget_on=budget_on,
+                         ratio=ratio, r_min=r_min,
+                         aggressive_elimination=aggressive_elimination,
+                         exhaust_budget=exhaust_budget)
         self.param_distributions = param_distributions
         self.n_iter = n_iter
 
--- fml/tests/test_search.py
+++ fml/tests/test_search.py
@@ -2,6 +2,7 @@ import pytest
 
 from sklearn.datasets import make_classification
 from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
 
 from fml.search import GridSuccessiveHalving, RandomSuccessiveHalving
 
@@ -10,12 +11,81 @@ parameters = {'kernel': ('linear', 'rbf'), 'C': [1, 10]}
 base_estimator = SVC(gamma='scale')
 
 
-@pytest.mark.parametrize('sh', (
-    GridSuccessiveHalving(base_estimator, parameters),
-    RandomSuccessiveHalving(base_estimator, parameters, n_iter=4)
-))
-def test_basic(sh):
-    X, y = make_classification(n_samples=1000, random_state=0)
-    sh.set_params(random_state=0, cv=5)
+# @pytest.mark.parametrize('sh', (
+#     GridSuccessiveHalving(base_estimator, parameters),
+#     RandomSuccessiveHalving(base_estimator, parameters, n_iter=4)
+# ))
+# def test_basic(sh):
+#     X, y = make_classification(n_samples=1000, random_state=0)
+#     sh.set_params(random_state=0, cv=5)
+#     sh.fit(X, y)
+#     assert sh.score(X, y) > .98
+
+
+
+def test_lol():
+    print('More than enough samples, exhaust_budget=True')
+    parameters = {'penalty': ('l1', 'l2'), 'C': [1, 10, 20]}
+    base_estimator = LogisticRegression(solver='liblinear')
+    X, y = make_classification(n_samples=1000)
+    sh = GridSuccessiveHalving(base_estimator, parameters, cv=5,
+                               random_state=0, exhaust_budget=False, ratio=3)
     sh.fit(X, y)
-    assert sh.score(X, y) > .98
+    assert sh.n_iterations_ ==  2
+    assert sh.n_required_iterations_ ==  2
+    assert sh.n_possible_iterations_ > 2
+    assert sh._r_i_list == [20, 60]
+
+    print()
+    print('-' * 10)
+    print('-' * 10)
+    print()
+
+    print('More than enough samples, exhaust_budget=True')
+    parameters = {'penalty': ('l1', 'l2'), 'C': [1, 10, 20]}
+    base_estimator = LogisticRegression(solver='liblinear')
+    X, y = make_classification(n_samples=1000)
+    sh = GridSuccessiveHalving(base_estimator, parameters, cv=5, random_state=0,
+                            exhaust_budget=True, ratio=3)
+    sh.fit(X, y)
+    assert sh.n_iterations_ ==  2
+    assert sh.n_required_iterations_ ==  2
+    assert sh.n_possible_iterations_ > 2
+    assert sh._r_i_list == [333, 999]
+
+    print()
+    print('-' * 10)
+    print('-' * 10)
+    print()
+
+    print('Not enough samples, finish before n_required_itearations')
+    parameters = {'penalty': ('l1', 'l2'), 'C': list(range(1, 20))}
+    base_estimator = LogisticRegression(solver='liblinear')
+    X, y = make_classification(n_samples=1000)
+    sh = GridSuccessiveHalving(base_estimator, parameters, cv=5, random_state=0,
+                               aggressive_elimination=False, max_budget=76,
+                               ratio=3)
+    sh.fit(X, y)
+    assert sh.n_iterations_ ==  2
+    assert sh.n_required_iterations_ ==  4
+    assert sh.n_possible_iterations_ == 2
+    assert sh._r_i_list == [20, 60]
+
+    print()
+    print('-' * 10)
+    print('-' * 10)
+    print()
+
+    print('Not enough samples (need to loop at the beginning), aggressive_elimination=True')
+    parameters = {'penalty': ('l1', 'l2'), 'C': list(range(1, 50))}
+    base_estimator = LogisticRegression(solver='liblinear')
+    X, y = make_classification(n_samples=1000)
+    sh = GridSuccessiveHalving(base_estimator, parameters, cv=5, random_state=0,
+                               aggressive_elimination=True, max_budget=190,
+                               ratio=3)
+    sh.fit(X, y)
+
+    assert sh.n_iterations_ ==  5
+    assert sh.n_required_iterations_ ==  sh.n_iterations_
+    assert sh.n_possible_iterations_ < sh.n_iterations_
+    assert sh._r_i_list == [20, 20, 20, 60, 180]

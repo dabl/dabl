d924f0d7bbde148f6e75326b4065f7398639f704
Andreas Mueller <andreas.mueller@columbia.edu>
2019-03-26 22:38:15 -0400

    put plot_supervised into to level
    add plot_boston example

--- examples/plot_test.py
+++
@@ -1,3 +0,0 @@
-"""THIS IS EMPTY
-
-"""
--- fml/__init__.py
+++ fml/__init__.py
@@ -1,4 +1,5 @@
 from .preprocessing import FriendlyPreprocessor
 from .models import FriendlyClassifier
+from .plotting import plot_supervised
 
-__all__ = ['FriendlyPreprocessor', 'FriendlyClassifier']
+__all__ = ['FriendlyPreprocessor', 'FriendlyClassifier', 'plot_supervised']

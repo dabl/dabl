2fa959e4a2aab1108241fc51cf9e22573beb09db
Andreas Mueller <andreas.mueller@columbia.edu>
2019-03-29 16:37:52 -0700

    import entry points in top level

--- dabl/__init__.py
+++ dabl/__init__.py
@@ -1,7 +1,9 @@
-from .preprocessing import EasyPreprocessor
+from .preprocessing import EasyPreprocessor, clean, detect_types
 from .models import SimpleClassifier
 from .plotting import plot_supervised
 from .search import GridSuccessiveHalving, RandomSuccessiveHalving
+from .explain import explain
 
-__all__ = ['EasyPreprocessor', 'SimpleClassifier', 'plot_supervised',
-           'GridSuccessiveHalving, RandomSuccessiveHalving']
+__all__ = ['EasyPreprocessor', 'SimpleClassifier', 'explain', 'clean',
+           'detect_types', 'plot_supervised', 'GridSuccessiveHalving',
+           'RandomSuccessiveHalving']

10cba91435fd978a254173c0b496fb9d36b8af15
Andreas Mueller <andreas.mueller@columbia.edu>
2019-04-08 10:17:14 -0400

    don't drop large cardinality integer in preprocessor

--- dabl/tests/test_preprocessing.py
+++ dabl/tests/test_preprocessing.py
@@ -259,8 +259,13 @@ def test_titanic_feature_names():
     ep.fit(clean(titanic.drop('survived', axis=1)))
     expected_names = [
         'age_dabl_continuous', 'body_dabl_continuous', 'fare_dabl_continuous',
-        'age_?_0.0', 'age_?_1.0', 'body_?_0.0', 'body_?_1.0', 'pclass_1',
-        'pclass_2', 'pclass_3', 'sex_female', 'sex_male', 'embarked_?',
+        'parch', 'sibsp', 'age_?_0.0', 'age_?_1.0', 'body_?_0.0', 'body_?_1.0',
+        'pclass_1',
+        'pclass_2', 'pclass_3', 'sex_female', 'sex_male', 'sibsp_0', 'sibsp_1',
+        'sibsp_2',
+        'sibsp_3', 'sibsp_4', 'sibsp_5', 'sibsp_8', 'parch_0', 'parch_1',
+        'parch_2',
+        'parch_3', 'parch_4', 'parch_5', 'parch_6', 'parch_9', 'embarked_?',
         'embarked_C', 'embarked_Q', 'embarked_S', 'boat_1', 'boat_10',
         'boat_11', 'boat_12', 'boat_13', 'boat_13 15', 'boat_13 15 B',
         'boat_14', 'boat_15', 'boat_15 16', 'boat_16', 'boat_2', 'boat_3',

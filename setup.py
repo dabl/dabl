bf3070453b065d06596695385ac5dd9c8ce83b74
Tim Head <betatim@gmail.com>
2019-04-15 23:26:54 +0200

    Add pandas as dependency and binder badge in readme

--- setup.py
+++ setup.py
@@ -1,25 +1,13 @@
 from __future__ import print_function
-import sys
 from setuptools import setup, find_packages
 
-
-try:
-    import numpy
-except ImportError:
-    print('numpy is required during installation')
-    sys.exit(1)
-
-try:
-    import scipy
-except ImportError:
-    print('scipy is required during installation')
-    sys.exit(1)
 
 setup(name='dabl',
       version='0.0.1',
       description='Data Analysis Baseline Library',
       author='Andreas Mueller',
       packages=find_packages(),
-      install_requires=[],
+      install_requires=["numpy", "scipy", "scikit-learn", "pandas",
+                        "matplotlib", "seaborn"],
       author_email='t3kcit+githubspam@gmail.com',
       )

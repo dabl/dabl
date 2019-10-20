58f913b7e4055b1ad1d1378852e94e72a09fd340
Andreas Mueller <andreas.mueller@columbia.edu>
2019-10-20 13:44:02 +0200

    prototype more plotting

--- dabl/explain.py
+++ dabl/explain.py
@@ -7,10 +7,12 @@ from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.inspection import plot_partial_dependence
 
 from .models import SimpleClassifier, SimpleRegressor, AnyClassifier
 from .utils import nice_repr, _validate_Xyt
-from .plot.utils import plot_coefficients, plot_multiclass_roc_curve
+from .plot.utils import (plot_coefficients, plot_multiclass_roc_curve,
+                         find_pretty_grid)
 
 
 def classification_metrics(estimator, X_val, y_val):
@@ -50,13 +52,16 @@ def explain(estimator, X_val=None, y_val=None, target_col=None,
     """
     if feature_names is None:
         try:
-            feature_names = estimator.feature_names_
+            feature_names = estimator.feature_names_.to_list()
         except AttributeError:
             raise ValueError("Can't determine input feature names, "
                              "please pass them.")
     classifier = False
     if hasattr(estimator, 'classes_') and len(estimator.classes_) >= 2:
+        n_classes = len(estimator.classes_)
         classifier = True
+    else:
+        n_classes = 1
 
     if X_val is not None:
         X_val, y_val = _validate_Xyt(X_val, y_val, target_col)
@@ -67,66 +72,80 @@ def explain(estimator, X_val=None, y_val=None, target_col=None,
         else:
             # FIXME
             pass
+        # FIXME Skip for linear models?
+        n_rows, n_cols = find_pretty_grid(len(feature_names))
+        print("Computing partial dependence plots...")
+        if n_classes <= 2:
+            plot_partial_dependence(estimator, X_val, features=feature_names,
+                                    feature_names=feature_names, n_cols=n_cols)
+        else:
+            for c in estimator.classes_:
+                plot_partial_dependence(estimator, X_val,
+                                        features=feature_names,
+                                        feature_names=feature_names,
+                                        target=c, n_cols=n_cols)
 
     # Start unpacking the estimator to get to the final step
-    if (isinstance(estimator, SimpleClassifier)
-            or isinstance(estimator, SimpleRegressor)):
+    inner_estimator = estimator
+    if (isinstance(inner_estimator, SimpleClassifier)
+            or isinstance(inner_estimator, SimpleRegressor)):
         # get the pipeline
-        estimator = estimator.est_
-    elif isinstance(estimator, AnyClassifier):
-        estimator = estimator.est_
-    if isinstance(estimator, Pipeline):
-        assert len(estimator.steps) == 2
+        inner_estimator = inner_estimator.est_
+    elif isinstance(inner_estimator, AnyClassifier):
+        inner_estimator = inner_estimator.est_
+    if isinstance(inner_estimator, Pipeline):
+        assert len(inner_estimator.steps) == 2
         # pipelines don't have feature names yet in sklearn
         # *cries in scikit-learn roadmap*
-        final_est = estimator._final_estimator
+        final_est = inner_estimator._final_estimator
         try:
-            feature_names = estimator.steps[0][1].get_feature_names(
+            feature_names = inner_estimator.steps[0][1].get_feature_names(
                 feature_names)
         except TypeError:
-            feature_names = estimator.steps[0][1].get_feature_names()
+            feature_names = inner_estimator.steps[0][1].get_feature_names()
 
         # now we have input feature names for the final step
-        estimator = final_est
+        inner_estimator = final_est
     # done unwrapping, start evaluating
 
-    if isinstance(estimator, DecisionTreeClassifier):
-        print(nice_repr(estimator))
+    if isinstance(inner_estimator, DecisionTreeClassifier):
+        print(nice_repr(inner_estimator))
         try:
-            print("Depth: {}".format(estimator.get_depth()))
-            print("Number of leaves: {}".format(estimator.get_n_leaves()))
+            print("Depth: {}".format(inner_estimator.get_depth()))
+            print("Number of leaves: {}".format(
+                inner_estimator.get_n_leaves()))
         except AttributeError:
             warn("Can't show tree depth, install scikit-learn 0.21"
                  " to show the full information.")
         # FIXME !!! bug in plot_tree with integer class names
-        class_names = [str(c) for c in estimator.classes_]
+        class_names = [str(c) for c in inner_estimator.classes_]
         plt.figure(figsize=(18, 10))
-        plot_tree(estimator, feature_names=feature_names,
+        plot_tree(inner_estimator, feature_names=feature_names,
                   class_names=class_names, filled=True, max_depth=5,
                   precision=2, proportion=True)
         # FIXME This is a bad thing to show!
-        plot_coefficients(estimator.feature_importances_, feature_names)
+        plot_coefficients(inner_estimator.feature_importances_, feature_names)
         plt.ylabel("Impurity Decrease")
-    elif hasattr(estimator, 'coef_'):
+    elif hasattr(inner_estimator, 'coef_'):
         # probably a linear model, can definitely show the coefficients
         # would be nice to have the target name here
-        if hasattr(estimator, "classes_"):
-            coef = np.atleast_2d(estimator.coef_)
-            for k, c in zip(estimator.classes_, coef):
+        if hasattr(inner_estimator, "classes_"):
+            coef = np.atleast_2d(inner_estimator.coef_)
+            for k, c in zip(inner_estimator.classes_, coef):
                 plot_coefficients(c, feature_names,
                                   classname="class: {}".format(k))
         else:
-            coef = np.squeeze(estimator.coef_)
+            coef = np.squeeze(inner_estimator.coef_)
             if coef.ndim > 1:
                 raise ValueError("Don't know how to handle "
                                  "multi-target regressor")
             plot_coefficients(coef, feature_names)
-    elif isinstance(estimator, RandomForestClassifier):
+    elif isinstance(inner_estimator, RandomForestClassifier):
         # FIXME This is a bad thing to show!
 
-        plot_coefficients(estimator.feature_importances_, feature_names)
+        plot_coefficients(inner_estimator.feature_importances_, feature_names)
         plt.ylabel("Imputity Decrease")
 
     else:
         raise ValueError("Don't know how to explain estimator {} "
-                         "yet.".format(estimator))
+                         "yet.".format(inner_estimator))

853398b92d6aa0ee8a2426eaded97a9c346826ba
Andreas Mueller <andreas.mueller@columbia.edu>
2019-11-04 11:11:09 -0800

    YES!

--- ci_scripts/travis/install.sh
+++ ci_scripts/travis/install.sh
@@ -33,7 +33,7 @@ then
     dev_url=https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com
     pip install --pre --upgrade --timeout=60 -f $dev_url scikit-learn
 else
-    conda install scikit-learn=$SKLEARN_VERSION
+    conda install --yes scikit-learn=$SKLEARN_VERSION
 fi
 
 

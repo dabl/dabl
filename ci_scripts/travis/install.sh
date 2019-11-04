10752a69ab0826a7a5de6465c41655355e9c6f43
Andreas Mueller <andreas.mueller@columbia.edu>
2019-11-04 10:59:00 -0800

    fix if else lol

--- ci_scripts/travis/install.sh
+++ ci_scripts/travis/install.sh
@@ -30,10 +30,10 @@ source activate testenv
 
 if [[ "$SKLEARN_VERSION" == "dev" ]]
 then
-    conda install scikit-learn=$SKLEARN_VERSION
-else
     dev_url=https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com
     pip install --pre --upgrade --timeout=60 -f $dev_url scikit-learn
+else
+    conda install scikit-learn=$SKLEARN_VERSION
 fi
 
 

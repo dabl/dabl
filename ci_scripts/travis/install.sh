d904f5a741bd9dd649b3b4b08df44918ab2bf053
Andreas Mueller <andreas.mueller@columbia.edu>
2019-11-04 11:23:30 -0800

    actually use sklearn nightly

--- ci_scripts/travis/install.sh
+++ ci_scripts/travis/install.sh
@@ -30,7 +30,7 @@ source activate testenv
 
 if [[ "$SKLEARN_VERSION" == "dev" ]]
 then
-    dev_url=https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com
+    dev_url=sklearn-nightly.scdn8.secure.raxcdn.com
     pip install --pre --upgrade --timeout=60 -f $dev_url scikit-learn
 else
     conda install --yes scikit-learn=$SKLEARN_VERSION

955e885e48182677ed20ef936d4ad66c13add29a
Andreas Mueller <andreas.mueller@columbia.edu>
2019-11-04 22:10:24 -0700

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='dabl',
-      version='0.1.0',
+      version='0.1.1',
       description='Data Analysis Baseline Library',
       author='Andreas Mueller',
       packages=find_packages(),

75d65b7ff4704f4556a4152da69df185c6e4a611
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-11-15 15:18:44 +0100

    DOC fix issue with autosummary with RTD and numpydoc

--- doc/conf.py
+++ doc/conf.py
@@ -313,3 +313,4 @@ sphinx_gallery_conf = {
 def setup(app):
     # a copy button to copy snippet of code from the documentation
     app.add_javascript('js/copybutton.js')
+    app.add_stylesheet("basic.css")

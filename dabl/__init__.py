26164e9d2abc62d1ed8a735c15ea5a46c50bba49
Andreas Mueller <andreas.mueller@columbia.edu>
2019-12-04 11:40:51 -0500

    Add AnyClassifier to main namespace

--- dabl/__init__.py
+++ dabl/__init__.py
@@ -1,10 +1,11 @@
 from .preprocessing import EasyPreprocessor, clean, detect_types
-from .models import SimpleClassifier, SimpleRegressor
+from .models import SimpleClassifier, SimpleRegressor, AnyClassifier
 from .plot.supervised import plot
 from .explain import explain
 from . import datasets
 
 __version__ = "0.1.2"
 
-__all__ = ['EasyPreprocessor', 'SimpleClassifier', 'SimpleRegressor',
+__all__ = ['EasyPreprocessor', 'SimpleClassifier', 'AnyClassifier',
+           'SimpleRegressor',
            'explain', 'clean', 'detect_types', 'plot', 'datasets']

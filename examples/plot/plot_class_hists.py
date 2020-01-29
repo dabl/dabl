73c909cbfb429b5ec3c295500439b3fcf3e62750
j1c <jaewonc78@gmail.com>
2020-01-29 11:06:20 -0500

    Remove sphinx option and add comment

--- examples/plot/plot_class_hists.py
+++ examples/plot/plot_class_hists.py
@@ -2,11 +2,12 @@
 Class Histogram Example
 ==========================================
 """
-# sphinx_gallery_thumbnail_number = 1
 import matplotlib.pyplot as plt
 from dabl.datasets import load_adult
 from dabl.plot import class_hists
 
 data = load_adult()
+
+# Plots the histogram of age per gender
 class_hists(data, "age", "gender", legend=True)
 plt.show()

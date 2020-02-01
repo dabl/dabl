e6d157e0106b3831b2500b170b455ab3115622b6
Parth Pankaj Tiwary <parthpankajtiwary@Parths-MacBook-Pro.local>
2020-02-01 23:29:06 +0530

    minor language change suggestion incorporated #177

--- dabl/utils.py
+++ dabl/utils.py
@@ -32,7 +32,7 @@ def _validate_Xyt(X, y, target_col, do_clean=True):
     if ((y is None and target_col is None)
             or (y is not None) and (target_col is not None)):
         raise ValueError(
-            "Need to specify exactly one of y and target_col.")
+            "Need to specify either y or target_col.")
     if do_clean:
         X = clean(X)
     if target_col is not None:

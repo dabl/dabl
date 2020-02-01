1889abd48eabc06ed1bddf075c0589f8dbeb5f2f
Andreas Müller <t3kcit@gmail.com>
2020-02-01 12:56:53 -0500

    add sphinx extensions from sklearn/imblearn make code link to github

--- doc/conf.py
+++ doc/conf.py
@@ -16,6 +16,8 @@ import sys
 import os
 
 import sphinx_rtd_theme
+sys.path.insert(0, os.path.abspath('sphinxext'))
+from github_link import make_linkcode_resolve
 import dabl
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -36,7 +38,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.viewcode',
+    'sphinx.ext.linkcode',
     'numpydoc',
     'sphinx_gallery.gen_gallery',
     'matplotlib.sphinxext.plot_directive'
@@ -313,3 +315,10 @@ sphinx_gallery_conf = {
 def setup(app):
     # a copy button to copy snippet of code from the documentation
     app.add_javascript('js/copybutton.js')
+
+
+# The following is used by sphinx.ext.linkcode to provide links to github
+linkcode_resolve = make_linkcode_resolve('dabl',
+                                         'https://github.com/dabl/'
+                                         'dabl/blob/{revision}/'
+                                         '{package}/{path}#L{lineno}')

d6ea5f3f693be1a8afe691eb8284f2e39f77357e
Shathra <selcuk.gulcan@bilkent.edu.tr>
2020-02-10 14:57:21 +0300

    test case added for issue 211

--- dabl/tests/test_preprocessing.py
+++ dabl/tests/test_preprocessing.py
@@ -360,3 +360,14 @@ def test_easy_preprocessor_transform():
     pipe.fit(X_train, y_train)
     pipe.predict(X_train)
     pipe.predict(X_val)
+
+
+def test_simple_preprocessor_imputed_features():
+    # Issue: 211
+
+    data = pd.DataFrame({'A': [0, 1, 2, 1, np.NaN]}, dtype=int)
+    types = detect_types(data, type_hints={'A': 'categorical'})
+
+    ep = EasyPreprocessor(types=types)
+    ep.fit(data)
+    ep.get_feature_names()

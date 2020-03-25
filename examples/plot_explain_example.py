46c000947c24e26bbfd06f959117f206f35b70ea
hp2500 <heinrichpeterz@gmail.com>
2020-03-25 14:08:20 -0400

    fixed data import

--- examples/plot_explain_example.py
+++ examples/plot_explain_example.py
@@ -4,9 +4,11 @@ Model Explanation
 """
 from dabl.models import SimpleClassifier
 from dabl.explain import explain
-from dabl.datasets import load_wine
+from dabl.utils import data_df_from_bunch
+from sklearn.datasets import load_wine
 
-wine_df = load_wine()
+wine = load_wine()
+wine_df = data_df_from_bunch(wine)
 
 sc = SimpleClassifier()
 sc.fit(wine_df, target_col='target')

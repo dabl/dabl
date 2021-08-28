dbe3ff9ba1794da22cd58fa16fa6a1af40fac66b
Mahima Mittal <mahimamittal829@gmail.com>
2021-08-28 19:40:39 +0530

    partial dependence

--- examples/plot/plot_diabetes.py
+++ examples/plot/plot_diabetes.py
@@ -4,16 +4,15 @@ Diabetes Dataset Visualization
 ==========================================
 """
 import pandas as pd
-from sklearn import datasets           
+from sklearn import datasets
 from sklearn.inspection import plot_partial_dependence
 from sklearn.experimental import enable_hist_gradient_boosting
 from sklearn.ensemble import HistGradientBoostingRegressor
 
 diabetes = datasets.load_diabetes()
 
-X = pd.DataFrame(diabetes.data, columns= diabetes.feature_names)
+X = pd.DataFrame(diabetes.data, columns = diabetes.feature_names)
 y = diabetes.target
-
 
 est = HistGradientBoostingRegressor()
 est.fit(X, y)
@@ -22,8 +21,6 @@ for i in range(0, 10):
   display = plot_partial_dependence(est, X, [i])
 
 for i in range(0, 10):
-    for j in range(0, 10):
-        if(i < j):
-            display = plot_partial_dependence(est, X, [(i, j)])
-
-
+  for j in range(0, 10):
+    if(i < j):
+      display = plot_partial_dependence(est, X, [(i, j)])

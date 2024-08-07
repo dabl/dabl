83374ec7f4c94146c1c3a826a0f9d028b618bd44
Brian Kroth <bpkroth@microsoft.com>
2024-08-07 15:02:46 -0500

    Check the build

---
+++ scripts/check_build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Be strict and verbose.
+set -euxo pipefail
+
+# Move to dabl repo root.
+scriptdir=$(dirname "$(readlink -f "$0")")
+cd "$scriptdir/.."
+
+python3 -m pip install build
+rm -rf build/ dist/
+python3 -m build
+# Basic validation of the whl produced.
+unzip -l dist/dabl-*-py3-none-any.whl | grep dabl/__init__.py
+# Make sure tests and docs aren't included.
+! (unzip -l dist/dabl-*-py3-none-any.whl | grep dabl/test )
+! (unzip -l dist/dabl-*-py3-none-any.whl | grep dabl/doc )
+# Make sure the package metadata looks right.
+unzip -p dist/dabl-*-py3-none-any.whl dabl-*/METADATA | grep 'pip install dabl'
+# Make sure the source tar looks OK.
+tar tzvf dist/dabl-*.tar.gz | grep examples/
+
+echo "OK"
